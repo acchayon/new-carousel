5e73fea7f4d8edbe4975aa420490aefb1c309c47
acchayon <adhikaryac81@gmail.com>
2023-03-18 22:18:09 +0600

    first commit

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,22 @@
 const images = [
-    'images/pic-1.jpg', // 0
-    'images/pic-2.jpg', // 1
-    'images/pic-3.jpg',
-    'images/pic-4.jpg',
-    'images/pic-5.jpg',
-    'images/pic-6.jpg',
-    'images/pic-7.jpg',
-    'images/pic-8.jpg',
-    'images/pic-9.jpg', // 8
-]
-
+    "images/pic-1.jpg",
+    "images/pic-2.jpg",
+    "images/pic-3.jpg",
+    "images/pic-4.jpg",
+    "images/pic-5.jpg",
+    "images/pic-6.jpg",
+    "images/pic-7.jpg",
+    "images/pic-8.jpg",
+    "images/pic-9.jpg",
+];
+const imgSlide = document.getElementById('img-slide')
 let imgIndex = 0;
-const imgEl = document.getElementById('slider-img');
-setInterval( () => {
+setInterval(() => {
     if(imgIndex === images.length){
-        imgIndex = 0;
+        imgIndex = 0
     }
-    const imgUrl = images[imgIndex];
-    // console.log(imgIndex, imgUrl);
-    imgEl.setAttribute('src', imgUrl);
-    imgIndex++;
-} , 1000)
+    const imgUrl = images[imgIndex]
+    // console.log(imgIndex,imgUrl);
+    imgSlide.setAttribute('src', imgUrl)
+    imgIndex++
+}, 1000)
